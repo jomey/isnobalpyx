fd46f2cdc7bd6afb80091b1647caa29e4e4b693f
Joachim Meyer <jomey@uw.edu>
2023-11-25 15:07:52 -0700

    Add Catch2 testing framework with first tests.
    Start testing the model code with the Catch2 framework. This also sets
    up the building the code with CMake.

---
+++ tests/_main.cpp
@@ -0,0 +1,5 @@
+//
+// Run entire test suite
+//
+#define CATCH_CONFIG_MAIN  // Catch provided main()
+#include <catch2/catch_test_macros.hpp>
---
+++ tests/test_net_radiation.cpp
@@ -0,0 +1,27 @@
+//
+//
+//
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+extern "C" {
+  #include "../libisnobalpyx/net_radiation.h"
+}
+
+TEST_CASE( "Calculating net radiation" ) {
+  SECTION("No incoming energy") {
+    REQUIRE( net_radiation(0, 0, 0) == 0.0 );
+  }
+  SECTION("Incoming shortwave") {
+    REQUIRE( net_radiation(100.0, 0, 0) == 100.0 );
+  }
+  SECTION("Incoming longwave") {
+    REQUIRE( net_radiation(0, 200.0, 0) == 196.0 );
+  }
+  SECTION("Snow Temperature") {
+    REQUIRE_THAT(
+        net_radiation(0, 0, 263.15),
+        Catch::Matchers::WithinRel(-266.469, 0.001)
+    );
+  }
+}
