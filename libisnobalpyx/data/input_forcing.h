931c6e3cd2b4c9f6115fc2562b8b2760c4ea0878
Joachim Meyer <jmeyer@boisestate.edu>
2024-03-03 14:54:49 -0700

    Start InputForcing struct
    Data struct to hold input forcing values for the current time step.

---
+++ libisnobalpyx/data/input_forcing.h
@@ -0,0 +1,14 @@
+//
+// Data structure holding input forcing values for the
+// current time step
+//
+
+#ifndef DATA_INPUT_FORCING_H
+#define DATA_INPUT_FORCING_H
+
+struct InputForcing {
+  double net_solar;
+  double longwave;
+};
+
+#endif // DATA_INPUT_FORCING_H
