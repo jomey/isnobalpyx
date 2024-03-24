0a37cde3457c25774a4b9c7adfd0f87d9c7057b0
Joachim Meyer <jmeyer@boisestate.edu>
2024-03-24 14:31:53 -0600

    Flux - Expand struct to calculate turbulent fluxes.

--- libisnobalpyx/data/flux.h
+++ libisnobalpyx/data/flux.h
@@ -1,9 +1,18 @@
 #ifndef MASS_ENERGY_FLUX_H
 #define MASS_ENERGY_FLUX_H
 
+struct MassFlux {
+  /* Mass flux by evaporation into air from active layer (kg/m^2/s) */
+  double evaporation_air;
+};
+
 struct EnergyFlux {
   /* Net allwave radiation (W/m^2) */
   double net_radiation;
+  /* Sensible heat transfer from surface (W/m^2) */
+  double sensible_heat;
+  /* Latent heat transfer from surface (W/m^2) */
+  double latent_heat;
 };
 
 #endif // MASS_ENERGY_FLUX_H
