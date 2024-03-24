1c70314e826eb2b65e0d4df16bbe207c7955890d
Joachim Meyer <jmeyer@boisestate.edu>
2024-03-24 14:29:42 -0600

    InputForcing - Expand struct to calculate turbulent fluxes.

--- libisnobalpyx/data/input_forcing.h
+++ libisnobalpyx/data/input_forcing.h
@@ -1,14 +1,22 @@
 //
 // Data structure holding input forcing values for the
-// current time step
+// current time step. Terms defined as 'const' are provided as forcing inputs
+// to the model. Other terms are calculated at the beginning of the time step.
 //
 
 #ifndef DATA_INPUT_FORCING_H
 #define DATA_INPUT_FORCING_H
 
 struct InputForcing {
-  double net_solar;
-  double longwave;
+  const double net_solar;
+  const double longwave;
+
+  const double air_temperature;
+  double air_pressure;
+  // TODO - Remove logic that changes this value in turbulent_energy.c
+  double vapor_pressure;
+
+  const double wind_speed;
 };
 
 #endif // DATA_INPUT_FORCING_H
